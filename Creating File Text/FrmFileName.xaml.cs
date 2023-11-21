4bbc2ac9fc76288ae4674d778b76e0540618438a
Jason Chanliongco Maliquid <jasonm2822@gmail.com>
2023-11-21 14:58:28 +0800

    Add files via upload

---
+++ Creating File Text/FrmFileName.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+
+namespace Creating_File_Text
+{
+    public partial class FrmFileName : Window
+    {        
+        public static string SetFileName;
+
+        public FrmFileName()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOkay_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtFileName.Text))
+            {
+                MessageBox.Show("Plese Enter A File Name!", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                Console.WriteLine("File Name Is Required");
+            }
+            else
+            {
+                MessageBox.Show("Your Text File Has Been Save And The File Location Is In Your Documents", "Save Path", MessageBoxButton.OK, MessageBoxImage.Information);
+                string fileName = TxtFileName.Text;
+                SetFileName = fileName + ".txt";
+                this.Close();
+
+               //This Is For The Challenge
+                FrmRegistration fr = new FrmRegistration();
+                fr.Show();
+            }
+        }
+
+    }
+}
---
+++ Creating File Text/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Creating_File_Text
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtInput.Text))
+            {
+                MessageBox.Show("Please Input!", "Warning",MessageBoxButton.OK,MessageBoxImage.Warning);
+                Console.WriteLine("Input Is Required");
+            }
+            else
+            {
+            FrmFileName Fn = new FrmFileName();
+            Fn.ShowDialog();
+            string getInput;
+            getInput = TxtInput.Text;
+            
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(docPath, FrmFileName.SetFileName)))
+            {
+                outputFile.WriteLine(getInput);
+                Console.WriteLine(getInput);
+            }
+            }
+
+        }
+    }
+}
